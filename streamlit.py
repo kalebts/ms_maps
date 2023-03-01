7e0c43787d63bcbd467bba0bfbfb29c4656747da
kalebts <43525156+kalebts@users.noreply.github.com>
2023-03-01 10:05:41 -0500

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from streamlit import components
+
+st.set_page_config(layout="wide") # make app wide
+
+page = st.sidebar.selectbox('Select page',
+  ['Hinds','Delta Counties', 'Claiborne and Jefferson Counties'])
+
+# st.title('MS Maps')
+
+if (page=='Hinds'):
+    st.title('Hinds Census Tracts')
+    components.v1.html(open('mississippi/maps/hinds_tracts_map.html').read(), height=700)
+
+if (page=='Delta Counties'):
+    st.title('Delta Counties')
+    components.v1.html(open('mississippi/maps/delta_counties_map.html').read(), height=700)
+
+if (page=='Claiborne and Jefferson Counties'):
+    st.title('Claiborne and Jefferson Counties')
+    components.v1.html(open('mississippi/maps/other_counties_map.html').read(), height=700)
+
